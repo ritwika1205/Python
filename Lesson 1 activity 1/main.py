e2956e19ac2d50d0c08d07060ffb1c4ef53392e8
ritwika1205 <ritwika.berry@gmail.com>
2025-10-12 08:27:41 +0530

    commit

---
+++ Lesson 1 activity 1/main.py
@@ -0,0 +1,14 @@
+name=input("enter your name:")
+age=int(input("enter your age"))
+weight=float(input("enter your weight:"))
+student=bool(input("you are a student true/false"))
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(student))
+age=str(age)
+print(type(age))
+weight=int(weight)
+print(type(weight))
+
+
