44527a3b8e1939d7a2678a8e6a796b3c0fdd093b
ritwika1205 <ritwika.berry@gmail.com>
2025-10-12 08:42:48 +0530

    commit

---
+++ Lession1 activity 2/activity 2.py
@@ -0,0 +1,14 @@
+a=10
+b=20
+add=a+b
+print("the addition is",add)
+sub=a-b
+print("substraction is",sub)
+mul=a*b
+print("the multiplication is",mul)
+div=a/b
+print("the division is",div)
+mod=a%b
+print("the modulus is",mod)
+exp=a**b
+print("the exponent is",exp)
