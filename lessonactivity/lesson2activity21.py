d0142e10d98a3b5a866e7a94a62cc493b21f2e14
ritwika1205 <ritwika.berry@gmail.com>
2026-01-14 08:42:57 +0530

    commit

---
+++ lessonactivity/lesson2activity21.py
@@ -0,0 +1,12 @@
+file_read= open('lessonactivity\Codingal.txt', 'r')
+print("File in read mode")
+print(file_read.read())
+file_read.close()
+file_write= open('lessonactivity\Codingal.txt', 'w')
+file_write.write("File in writing mode......")
+file_write.write("Hi! I am a penguin and I am just 1 year old")
+file_write.close()
+file_append= open('lessonactivity\Codingal.txt', 'a')
+file_append.write("\n File in append mode")
+file_append.write("Hi! I am a penguin and I am just 1 year old")
+file_append.close
