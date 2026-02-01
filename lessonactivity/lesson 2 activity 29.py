735423faac6f0b01ee7cc031e243bc491264d53c
ritwika1205 <ritwika.berry@gmail.com>
2026-02-01 08:26:00 +0530

    commit

--- lessonactivity/lesson 2 activity 29.py
+++ lessonactivity/lesson 2 activity 29.py
@@ -1,4 +1,18 @@
-with open("lessonactivity\sample_doc (1).txt", "r") as file:
-    data= file.readlines()
-print(data)
+from tkinter import*
+window= Tk()
+window.title('Tkinter sample window')
+window.geometry('500x300')
+greeting= Label(text="Hello user", fg= 'black', bg= 'white')
+button= Button(text= "Click me", bg= 'black', fg= 'white')
+entry= Entry(fg= "yellow", bg= "blue", width= 50)
+greeting.pack()
+button.pack()
+entry.pack()
+frame= Frame(master= window, relief= RAISED, borderwidth= 5)
+frame.pack()
+label= Label(master= frame, text= 'Sample Frame')
+label.pack()
+textbook= Text(fg= 'green', bg= 'yellow')
+textbook.pack()
+window.mainloop()
     
